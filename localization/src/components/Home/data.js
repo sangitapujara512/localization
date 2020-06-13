f66b073b780d97380773f1d32a1eeef0a1690ea3
sangitapujara512 <sangitapujara512@gmail.com>
2020-06-13 05:40:33 +0530

    First commit
    first commit

---
+++ localization/src/components/Home/data.js
@@ -0,0 +1,10 @@
+const demoJson = {
+	"demo": {
+		"title": {
+			"pt": "Olá Mundo",
+			"en": "Hello World",
+			"es": "Hola Mundo"
+		},				
+	}
+};
+export default  demoJson
---
+++ localization/src/components/Home/index.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import demoJson from './data';
+const title=demoJson.demo.title;
+
+export default class componentName extends Component {
+    constructor(){
+        super();
+        this.state={
+            localLanguage: 'en',            
+        }
+    }
+    
+    handleSelect=(e)=>{        
+        this.setState({
+            localLanguage: e.target.value,
+        });
+    }
+    render() {
+        const {localLanguage}=this.state;
+        const showTitle= localLanguage === 'en'? demoJson.demo.title.en: localLanguage === 'pt'? demoJson.demo.title.pt :demoJson.demo.title.es;         
+        
+        return (
+            <div>
+                 <div>
+		    	<div>
+                    <p>Please select language.</p>
+                    <select onChange={this.handleSelect} className='selectStyle'>  
+                    <option defaultValue="en">English</option>                  
+                    <option value='pt'>Português</option>                   
+                    <option value="es">Español</option>
+                    </select>
+				      
+			    </div>
+			    <h1 >{showTitle}</h1>			   		   
+		  </div>                
+            </div>
+        )
+    }
+}
